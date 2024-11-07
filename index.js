5ebcc53da0163da96541286e3388f6b92b6cbf31
Ayush karwa <ayushkarwa18@gmail.com>
2024-11-08 02:26:54 +0530

    week 7

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require("express")
+const dotenv = require("dotenv").config()
+const {JWT_SECRET,auth} = require("./auth.js")
+const jwt = require("jsonwebtoken")
+const {UserModel,TodoModel} = require("./db.js")
+const app = express()
+const mongoose = require("mongoose")
+mongoose.connect(process.env.MONGODB_URI).
+then(()=>{
+    console.log("Database Connected...");
+    
+})
+app.use(express.json());
+
+
+
+
+app.post("/signup",async(req,res)=>{
+
+    const {email,password,name} = req.body
+
+    await UserModel.create({
+        email: email,
+        password: password,
+        name: name
+    })
+    res.json({
+        message:"you signed up successfully!"
+    })
+})
+
+app.post("/signin",async(req,res)=>{
+
+    const {email,password} = req.body
+
+    const user = await UserModel.findOne({
+        email:email,
+        password:password
+    })
+    console.log(user);
+
+    if(user){
+        console.log("the user id: "+user._id.toString())
+        const token = jwt.sign({
+            id: user._id.toString()
+        },JWT_SECRET)
+        res.json({
+            message:"You signed in succesfully!",
+            token: token
+        })
+    }else{
+        res.status(403).json({
+            message: "Incorrect Credentials"
+        })
+    }
+
+})
+
+app.post("/todo",auth,async(req,res)=>{
+
+    const {title,done}= req.body
+    const userId = req.userId
+    
+    await TodoModel.create({
+        title:title,
+        done: done,
+        userId: userId
+
+    })
+
+    
+    res.json({
+        message: "Todo created successfully!"
+    })
+    
+})
+
+app.get("/todos",auth,async(req,res)=>{
+
+    const userId = req.userId
+
+    const todos = await TodoModel.findOne({
+        userId:userId
+    })
+    if(todos){ 
+    console.log("This are the todos: "+todos);
+    console.log(JSON.stringify(todos.title,todos.done))
+    res.json({
+        todo: {
+            title: todos.title,
+            status: todos.done
+        }
+    })
+    }else{
+        res.json({
+            message:"todo does not exist"
+        })
+    }
+    
+})
+
+app.listen(3000,()=>{
+    console.log("listening on port 3000...")
+})
